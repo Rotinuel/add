036643ba1c49b76bbeb5d1f2b38c051c1244bbe1
xalphi <emmanuelisagenius@gmail.com>
2025-10-22 16:12:13 +0100

    checking

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,22 +1,19 @@
 // components/EventCard.tsx
 import { FC } from 'react';
 import { CalendarDays, MapPin, Users } from 'lucide-react';
-import { BiCategory } from 'react-icons/bi';
 
 interface EventCardProps {
   sdate: string;
   edate: string;
-  type: string;
   title: string;
   venue: string;
   bgColor?: string;
-  capacity: number;
   isPlaceholder?: boolean;
   price: number;
   description: string;
 }
 
-const EventCard: FC<EventCardProps> = ({ sdate, edate, type, title, venue, capacity, bgColor = "bg-white", isPlaceholder, price, description }) => {
+const EventCard: FC<EventCardProps> = ({ sdate, edate,  title, venue, bgColor = "bg-white", isPlaceholder, price, description }) => {
   if (isPlaceholder) {
     return (
       <div className="w-full h-56 bg-gray-300 rounded-xl flex items-center justify-center text-gray-600">
@@ -31,8 +28,6 @@ const EventCard: FC<EventCardProps> = ({ sdate, edate, type, title, venue, capac
       <h2 className="text-xl font-semibold mb-4 flex items-center gap-2 text-gray-900">
         <span role="img" aria-label="icon">🏢</span> {title}
       </h2>
-      {/* Calender */}
-      <p className="text-sm mb-2">{type}</p>
 
       {/* price */}
       <div className="bg-green-700 text-white text-center text-2xl font-bold py-3 rounded-md mb-4">
@@ -46,18 +41,9 @@ const EventCard: FC<EventCardProps> = ({ sdate, edate, type, title, venue, capac
           <MapPin className='w-4 h-4 mr-2' />
           {venue}
         </li>
-        {/* categories */}
-        <li className="flex items-center gap-2">
-          <BiCategory className='w-4 h-4 mr-2' />
-          {type}
-        </li>
       <li className="flex items-center mb-4">
         <CalendarDays className="mr-2" />
         <span className="text-sm">{sdate} - {edate}</span>
-      </li>
-      <li className="flex items-center text-sm">
-        <Users className="w-4 h-4 mr-2" />
-        {capacity} capacity
       </li>
       </ul>
       
